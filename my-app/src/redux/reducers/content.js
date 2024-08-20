bf749c1dc254247d5d4ce6316f43a8b5a18f19db
aUngurian <a.ungurian@peppers-studio.ru>
2024-08-20 14:28:42 +0300

    init

---
+++ my-app/src/redux/reducers/content.js
@@ -0,0 +1,13 @@
+import {createSlice } from "@reduxjs/toolkit";
+
+const contentSlice = createSlice({
+    name: "content",
+    initialState: {
+    },
+    reducers: {
+
+    },
+});
+
+export const {} = contentSlice.actions;
+export default contentSlice.reducer;
